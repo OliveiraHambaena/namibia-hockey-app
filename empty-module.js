adb3a57c9a74fe798052c163841bbc057e23528b
Oliveira Hambaena <167088414+OliveiraHambaena@users.noreply.github.com>
2025-05-19 03:28:14 +0200

    delete

--- empty-module.js
+++
@@ -1,7 +0,0 @@
-// This is an empty module to satisfy Metro bundler when it tries to resolve tempo-devtools
-module.exports = {
-  init: () => {
-    console.log("Mock Tempo Devtools initialized");
-    return true;
-  },
-};
